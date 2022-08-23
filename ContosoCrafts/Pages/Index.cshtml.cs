9958a8c6c8f0ed2f5c6566e0fcbf5b22b3245422
SBM <SBM@SKM>
2022-08-24 00:03:05 +0530

    Add project files.

---
+++ ContosoCrafts/Pages/Index.cshtml.cs
@@ -0,0 +1,24 @@
+﻿using ContosoCrafts.Models;
+using ContosoCrafts.Services;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ContosoCrafts.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        private JsonFileProductService _productService;
+        public IEnumerable<Product>? Products { get; private set; }
+
+        public IndexModel(ILogger<IndexModel> logger, JsonFileProductService productService)
+        {
+            _logger = logger;
+            _productService = productService;
+        }
+
+        public void OnGet()
+        {
+            Products = _productService.GetProducts();
+        }
+    }
+}
---
+++ ContosoCrafts/Services/JsonFileProductService.cs
@@ -0,0 +1,35 @@
+﻿using ContosoCrafts.Models;
+using System.Text.Json;
+
+namespace ContosoCrafts.Services
+{
+    public class JsonFileProductService
+    {
+        public JsonFileProductService(IWebHostEnvironment webHostEnvironment)
+        {
+            WebHostEnvironment = webHostEnvironment;
+        }
+
+        public IWebHostEnvironment WebHostEnvironment { get; }
+
+        public string JsonFileName
+        {
+            get
+            {
+                return Path.Combine(WebHostEnvironment.WebRootPath, "data", "products.json");
+            }
+        }
+
+        public IEnumerable<Product>? GetProducts()
+        {
+            using (var jsonFileReader = File.OpenText(JsonFileName))
+            {
+                return JsonSerializer.Deserialize<Product[]>(jsonFileReader.ReadToEnd(),
+                    new JsonSerializerOptions
+                    {
+                        PropertyNameCaseInsensitive = true
+                    });
+            }
+        }
+    }
+}
